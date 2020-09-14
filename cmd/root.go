88615b3fa3d2ec44eb553a4ba94f5a29fa3b55f8
Tim Ling <tim.ling@silkrode.com.tw>
2020-09-14 16:53:39 +0800

    add root.go work & dockerfile

--- cmd/root.go
+++ cmd/root.go
@@ -16,12 +16,25 @@ limitations under the License.
 package cmd
 
 import (
+	"database/sql"
 	"fmt"
-	"github.com/spf13/cobra"
+	"log"
+	"net/url"
 	"os"
+	"time"
+
+	"github.com/labstack/echo"
+	"github.com/spf13/cobra"
 
 	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/viper"
+
+	_ "github.com/go-sql-driver/mysql"
+
+	_articleHttpDelivery "gitlab.silkrode.com.tw/team_golang/kbc2/sample/internal/pkg/delivery/http"
+	_articleHttpDeliveryMiddleware "gitlab.silkrode.com.tw/team_golang/kbc2/sample/internal/pkg/delivery/http/middleware"
+	_repo "gitlab.silkrode.com.tw/team_golang/kbc2/sample/internal/pkg/repository/mysql"
+	_articleUcase "gitlab.silkrode.com.tw/team_golang/kbc2/sample/internal/pkg/usecase"
 )
 
 var cfgFile string
@@ -38,7 +51,46 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
-	//	Run: func(cmd *cobra.Command, args []string) { },
+	Run: func(cmd *cobra.Command, args []string) {
+		dbHost := viper.GetString(`database.host`)
+		dbPort := viper.GetString(`database.port`)
+		dbUser := viper.GetString(`database.user`)
+		dbPass := viper.GetString(`database.pass`)
+		dbName := viper.GetString(`database.name`)
+		connection := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPass, dbHost, dbPort, dbName)
+		val := url.Values{}
+		val.Add("parseTime", "1")
+		val.Add("loc", "Asia/Jakarta")
+		dsn := fmt.Sprintf("%s?%s", connection, val.Encode())
+		dbConn, err := sql.Open(`mysql`, dsn)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = dbConn.Ping()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		defer func() {
+			err := dbConn.Close()
+			if err != nil {
+				log.Fatal(err)
+			}
+		}()
+
+		e := echo.New()
+		middL := _articleHttpDeliveryMiddleware.InitMiddleware()
+		e.Use(middL.CORS)
+		authorRepo := _repo.NewMysqlAuthorRepository(dbConn)
+		ar := _repo.NewMysqlArticleRepository(dbConn)
+
+		timeoutContext := time.Duration(viper.GetInt("context.timeout")) * time.Second
+		au := _articleUcase.NewArticleUsecase(ar, authorRepo, timeoutContext)
+		_articleHttpDelivery.NewArticleHandler(e, au)
+
+		log.Fatal(e.Start(viper.GetString("server.address")))
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
--- go.mod
+++ go.mod
@@ -3,8 +3,19 @@ module gitlab.silkrode.com.tw/team_golang/kbc2/sample
 go 1.14
 
 require (
+	github.com/bxcodec/faker v2.0.1+incompatible
+	github.com/go-playground/universal-translator v0.17.0 // indirect
+	github.com/go-sql-driver/mysql v1.5.0
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/leodido/go-urn v1.2.0 // indirect
 	github.com/mitchellh/go-homedir v1.1.0
+	github.com/sirupsen/logrus v1.2.0
 	github.com/spf13/cobra v1.0.0
 	github.com/spf13/viper v1.7.1
-	github.com/stretchr/testify v1.3.0
+	github.com/stretchr/testify v1.4.0
+	golang.org/x/sync v0.0.0-20190423024810-112230192c58
+	gopkg.in/DATA-DOG/go-sqlmock.v1 v1.3.0
+	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
+	gopkg.in/go-playground/validator.v9 v9.31.0
 )
